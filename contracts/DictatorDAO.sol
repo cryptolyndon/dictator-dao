5acd3a8438fab1006eeaa9fde0b34be720f68b14
Barry Lyndon <redmond@barrylyndon.dev>
2021-11-28 22:46:45 +1100

    Add price tests

--- contracts/DictatorDAO.sol
+++ contracts/DictatorDAO.sol
@@ -379,6 +379,7 @@ contract DictatorToken is ERC20, BoringBatchable {
         weekShares[week] += shares;
 
         uint256 tokensPerWeek = tokensPerWeek(currentWeek);
+        // Price is per "token ETH"; tokensPerWeek is in "token wei".
         require(weekShares[week].mul(1e18) <= currentPrice * tokensPerWeek, "Oversold");
     }
 
@@ -399,6 +400,8 @@ contract DictatorToken is ERC20, BoringBatchable {
         //      - shares        <= max_{all time}(price * tokensPerWeek)
         // the numerator is at most 1.8e54 < 2 * 2**180:
         uint256 tokens = (userWeekShares[to][week] * tokensPerWeek(week)) / weekShares[week];
+        // By int division and the fact that all tokens have been minted, the
+        // following will not underflow. TODO: check/enforce
         balanceOf[address(this)] -= tokens;
         balanceOf[to] += tokens;
         emit Transfer(address(this), to, tokens);
